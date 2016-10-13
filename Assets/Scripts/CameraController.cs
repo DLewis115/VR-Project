302d55b10500ee50f503a00d3d9df1f4a4f6f33e
Daniel Lewis <Lewis411@live.marshall.edu>
2016-10-13 00:23:41 -0400

    removed physics from player character to eliminate any headbob caused by the terrain causing motion sickness

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+    public float rotSpeed;
+
+    private Vector3 offset;
+
+	//Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+	}
+
+    void FixedUpdate()
+    {
+        float horizontal = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float vertical = speed * Input.GetAxis("Vertical") * Time.deltaTime;
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+        transform.Translate(horizontal, 0f, vertical);
+        transform.RotateAround(Vector3.up, rotX);
+
+    }
+}

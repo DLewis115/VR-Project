7a42603a8485ec988cb54a0e71d9b72bbbe6ac9e
Daniel <DLewis115@hotmail.com>
2016-10-12 20:05:49 -0400

    Player model can now move forward, backward, left, and right

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController: MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, Input.GetAxis ("Vertical") * Time.deltaTime);
+	}
+}

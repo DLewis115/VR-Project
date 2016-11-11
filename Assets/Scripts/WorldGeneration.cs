21a81264e5c35134b3019d65dd014bdaf14599ae
Daniel Lewis <Lewis411@live.marshall.edu>
2016-11-10 19:56:23 -0500

    re-wrote world generation script

---
+++ Assets/Scripts/WorldGeneration.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGeneration : MonoBehaviour {
+
+    public GameObject[] blocks = new GameObject[4];
+    const float BlockLength = 348f;
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject block in blocks)
+        {
+            Vector3 newBlockPos = block.transform.position;
+            newBlockPos.z -= moveSpeed * Time.deltaTime;
+            if (newBlockPos.z < -BlockLength / 4)
+            {
+                newBlockPos.z += BlockLength;
+            }
+            block.transform.position = newBlockPos;
+        }
+	}
+}

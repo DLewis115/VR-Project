5b1fd39bd47bd027ee044b6e111af246f9ae6b0c
Daniel Lewis <Lewis411@live.marshall.edu>
2017-01-10 01:15:17 -0500

    Most functionality for moon scene is now present. Need to build path around lunar surface for Cameras to follow

---
+++ Assets/Scripts/MoonMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonMovement : MonoBehaviour {
+
+    public float speed;
+    public float distance;
+    private Vector3[] thePath;
+    private float pathLength;
+ 
+    void Start()
+    {
+        thePath = iTweenPath.GetPath("MoonMovement");
+        pathLength = iTween.PathLength(thePath);
+    }
+
+    void Update()
+    {
+        distance += speed * Time.deltaTime;
+        float perc = distance / pathLength;
+        iTween.PutOnPath(gameObject, thePath, perc);
+        if (distance >= pathLength)
+        {
+            distance = 0;
+        }
+    }
+}

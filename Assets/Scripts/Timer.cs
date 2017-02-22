d114dab17db197e41db6b4f2e7443d67a5fc8144
Daniel Lewis <Lewis411@live.marshall.edu>
2017-02-22 18:36:33 -0500

    Changed range of values user could enter for rotating test subject. Also added timer.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text myTimer;
+    public float startTime;
+
+    void start()
+    {
+        startTime = Time.time;
+    }
+    
+    void update()
+    {
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        myTimer.text = minutes + ":" + seconds;
+        print(seconds);
+    }
+}

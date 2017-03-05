47b04d033c562c4c843d482c2a529a8f85301643
Daniel Lewis <Lewis411@live.marshall.edu>
2017-03-05 01:05:19 -0500

    Perturbation automation fully functioning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController: MonoBehaviour {
 
     bool rotating = false;
     bool rotated = false;
+    bool wait = false;
     bool executeRot = false;
     bool automate = false;
     bool automated = false;
@@ -25,14 +26,16 @@ public class PlayerController: MonoBehaviour {
             StartCoroutine(Automate(minTime, maxTime));
         }
 
-        if (!rotating && executeRot)
+        if (!rotating && executeRot && !wait)
         {
+            wait = !wait;
             StartCoroutine(Rotate(rotDegrees, axis, rotSpeed));
         }
-        if (!rotating && rotated)
+        if (!rotating && rotated && wait)
         {
             float rot = 0 - rotDegrees;
             StartCoroutine(Rotate(rot, axis, rotSpeed));
+            wait = !wait;
         }
 	}
 
@@ -63,46 +66,56 @@ public class PlayerController: MonoBehaviour {
 
         while (automate)
         {
+            while (wait)
+            {
+                yield return new WaitForSeconds(1);
+            }
             yield return new WaitForSeconds(UnityEngine.Random.Range(minTime, maxTime));
             rotDegrees = UnityEngine.Random.Range(-80, 80);
-            rotSpeed = UnityEngine.Random.Range(1, 180);
+            rotSpeed = UnityEngine.Random.Range(30, 180);
             execute();
-            yield return new WaitForSeconds(waitTime);
         }
 
         automated = false;
     }
 
+    //Used to set the degrees/s you want to rotate the camera
     public void setSpeed(float newSpeed)
     {
         rotSpeed = newSpeed;
     }
 
+    //Used to set the # of degrees you want to rotate the camera
     public void setDegree(float newDegree)
     {
         rotDegrees = newDegree;
     }
 
+    //Used to set how many seconds to keep the camera rotated before returning it to it's original position
     public void setWaitTime(float newTime)
     {
         waitTime = newTime/10;
     }
 
+    //Used to set the minimum number of seconds to take before the next rotation
     public void setMinTime(String newTime)
     {
         minTime = float.Parse(newTime);
     }
 
+    //Used to set the maximum number of seconds to take before the next rotation
     public void setMaxTime(String newTime)
     {
         maxTime = float.Parse(newTime);
     }
 
+    //Used to toggle having an random number generator control the camera rotations
     public void setAutomate()
     {
         automate = !automate;
     }
 
+    //Used to execute the camera rotation
     public void execute()
     {
         executeRot = true;
